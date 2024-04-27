4c7093b74f49ff5829db975749d2713a2ccf4af5
Matel <matel222@hotmail.com>
2024-04-27 17:05:20 +0700

    adding new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,7 +62,7 @@ export const routes: Routes = [
     MultiModuleComponent,
     FinalProjectComponent,
     ViewEncapsulationComponent,
-    AngularStylingComponent
+    AngularStylingComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/snippets/angular/form/form.component.ts
+++ src/app/snippets/angular/form/form.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
 
+  title ="Create a form"
+  task ="The task is to be able to create a form with validation"
+
+  donutSingle = `ng g c admin/containers/donut-single`;
+  donutForm = `ng g c admin/components/donut-form`;
+  formContent =`<form class="donut-form" #form="ngForm">
+</form>`
+  formInput =`<form class="donut-form" #form="ngForm">
+    <label>
+      <span>Name</span>
+      <input
+        type="text"
+        name="name"
+        class="input"
+        ngModel
+      />
+    </label>
+  </form>`
+
+
+
 }
